6af8e0c87edad7d68539ee63ba65e6aa2e87ec23
knagesh9841 <41575061+knagesh9841@users.noreply.github.com>
2018-08-15 00:21:56 +0530

    changed png to jpg

--- MobileTesting/src/utility/ScreenshotCapture.java
+++ MobileTesting/src/utility/ScreenshotCapture.java
@@ -19,7 +19,7 @@ public class ScreenshotCapture {
 		TakesScreenshot ts = (TakesScreenshot) driver;
 		File source = ts.getScreenshotAs(OutputType.FILE);
 		//after execution, you could see a folder "FailedTestsScreenshots" under src folder
-		String destination = System.getProperty("user.dir") + "/Screenshots/"+screenshotName+dateName+".png";
+		String destination = System.getProperty("user.dir") + "/Screenshots/"+screenshotName+dateName+".jpg";
 		File finalDestination = new File(destination);
 		FileUtils.copyFile(source, finalDestination);
 		//Returns the captured file path
